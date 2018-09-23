c595f2727401c53448e4e1288814bfee56c95894
Simon Chervenak <sarras305@gmail.com>
2018-09-23 16:38:07 -0400

    added linkifier

--- index.md
+++ index.md
@@ -1,6 +1,25 @@
 ---
 layout: default
 ---
-[ADSA Notes](/ADSA/placeholder.txt)
+[FCS Notes for placeholder](/FCS/placeholder.txt)
 
-[ADSB Notes](/ADSB/placeholder.txt)
+[ADSA Notes for placeholder](/ADSA/placeholder.txt)
+
+[Analysis 1A Notes for placeholder](/Analysis 1A/placeholder.txt)
+
+[Freshman Physics Notes for placeholder](/Freshman Physics/placeholder.txt)
+
+[Freshman Chem Notes for placeholder](/Freshman Chem/placeholder.txt)
+
+[ADSB Notes for placeholder](/ADSB/placeholder.txt)
+
+[Analysis 1B Notes for placeholder](/Analysis 1B/placeholder.txt)
+
+[AP World Notes for placeholder](/AP World/placeholder.txt)
+
+[NSL Notes for placeholder](/NSL/placeholder.txt)
+
+[AOA Notes for placeholder](/AOA/placeholder.txt)
+
+[Biology Notes for placeholder](/Biology/placeholder.txt)
+
---
+++ linkify.py
@@ -0,0 +1,26 @@
+import os
+
+def clear_index():
+    with open("index.md", "w"):
+        pass
+
+
+def find_all_notes(extensions=('pdf', 'doc', 'txt', 'md', 'png', 'jpg')):
+    for root, dirs, files in os.walk("."):
+        for file in files:
+            full = os.path.join(root, file)
+            if full.count("/") >= 2 and file.endswith(extensions) and not full.startswith("./_"):
+                yield full[1:]
+
+
+def write_all_file_links_to_index():
+    clear_index()
+    with open("index.md", "w") as index:
+        index.write("---\nlayout: default\n---\n")
+        for file in find_all_notes():
+            _, class_, noteName = file.split("/")
+            noteName, _ = noteName.split(".")
+            index.write(f"[{class_} Notes for {noteName}]({file})\n\n")
+
+
+write_all_file_links_to_index()
